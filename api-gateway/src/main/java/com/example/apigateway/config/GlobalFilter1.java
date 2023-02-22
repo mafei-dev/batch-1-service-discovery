cfa5f30fc5fea7c4c874714ce1b956ba30c34966
mafei <mafei.dev@gmail.com>
2023-02-22 22:24:16 +0530

    GlobalFilter order with implementing Ordered.

---
+++ api-gateway/src/main/java/com/example/apigateway/config/GlobalFilter1.java
@@ -0,0 +1,26 @@
+package com.example.apigateway.config;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class GlobalFilter1 implements GlobalFilter, Ordered {
+    @Override
+    public int getOrder() {
+        return 1;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("GlobalFilter1.filter:PRE");
+        //do something when the request goes down to the utility services
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            //do something when the request comes back
+            System.out.println("GlobalFilter1.filter:POST");
+        }));
+    }
+}
